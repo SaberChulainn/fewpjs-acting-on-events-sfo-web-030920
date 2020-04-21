81a503b5b5fa39ae4961c1893d5f585554c9022b
SaberChulainn <Tbandachit@gmail.com>
2020-04-21 11:08:44 -0700

    Done.

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ document.addEventListener("keydown", function(e) {
     console.log(e.key);
   });
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
- 
-    dodger.style.left = `${left - 1}px`;
-  }
-});
-
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
@@ -29,5 +20,19 @@ function moveDodgerLeft() {
   document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
     }
   });
